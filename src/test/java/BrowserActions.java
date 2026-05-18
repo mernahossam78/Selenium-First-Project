7066228b9cbf633f763603bd63b8304b3d7baae6
Merna <merahossam78@gmail.com>
2026-05-18 17:18:32 +0300

    navigate method

--- src/test/java/BrowserActions.java
+++ src/test/java/BrowserActions.java
@@ -1,3 +1,36 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
 public class BrowserActions {
+    WebDriver driver;
+
+    //Test runner
+    @Test
+    public void testCase() {
+        driver = new EdgeDriver();
+        navigateTo("http://google.com");
+        navigateTo("http://bing.com");
+        navigateBack();
+        navigateForward();
+        refresh();
+    }
+
+    public void navigateTo(String url) {
+        driver.navigate().to(url);
+    }
+
+    public void navigateBack() {
+        driver.navigate().back();
+    }
+
+    public void navigateForward() {
+        driver.navigate().forward();
+    }
+
+    public void refresh() {
+        driver.navigate().refresh();
+    }
+
 
 }
