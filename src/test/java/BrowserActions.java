8196e41298b21955587971d7a44e7d6810f2a050
Merna <merahossam78@gmail.com>
2026-05-21 17:14:42 +0300

    Browser Actions

--- src/test/java/BrowserActions.java
+++ src/test/java/BrowserActions.java
@@ -1,4 +1,7 @@
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
@@ -9,11 +12,27 @@ public class BrowserActions {
     @Test
     public void testCase() {
         driver = new EdgeDriver();
+        maximize();
+        //setPosition();
+        //setSize(430, 932);
+        //fullScreen();
         navigateTo("http://google.com");
+        driver.switchTo().newWindow(WindowType.TAB);
+        //close: closes the current window only
+        //close();
+        //quit, closes all the tabs
+        quit();
+        /*
         navigateTo("http://bing.com");
+        getCurrentUrl();
+        getTitle();
+        getPageSource();
         navigateBack();
         navigateForward();
-        refresh();
+        //refresh();
+        getWindowHandle();
+        */
+
     }
 
     public void navigateTo(String url) {
@@ -32,5 +51,55 @@ public class BrowserActions {
         driver.navigate().refresh();
     }
 
+    public void maximize() {
+        driver.manage().window().maximize();
+    }
 
+    public void setPosition() {
+
+        // Point point = new Point(100 , 200);
+        //ely t7t asmo anonamous object
+        driver.manage().window().setPosition(new Point(100, 200));
+    }
+
+    public void setSize(int width, int height) {
+        //Dimension dimension = new Dimension(width, height);
+        driver.manage().window().setSize(new Dimension(width, height));
+    }
+
+    public void fullScreen() {
+        driver.manage().window().fullscreen();
+    }
+
+    public void getCurrentUrl() {
+        String url = driver.getCurrentUrl();
+        System.out.println("Current URL: " + url);
+    }
+
+    public void getTitle() {
+        String title = driver.getTitle();
+        System.out.println(title);
+    }
+
+    public void getPageSource() {
+        String src = driver.getPageSource();
+        System.out.println("Page Source: " + src);
+    }
+
+    public void getWindowHandle() {
+        String handle = driver.getWindowHandle();
+        System.out.println("Window handle:" + handle);
+    }
+
+    public void close() {
+        driver.close();
+    }
+
+    public void quit() {
+        driver.quit();
+    }
+
+    public void scrollingUsingJS() {
+
+    }
 }
